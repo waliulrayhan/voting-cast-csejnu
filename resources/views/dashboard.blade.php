912755d249ff38c19342cae89c58847fd6d91ba9
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-08-06 20:37:25 +0600

    Home Page UI Ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
@@ -13,5 +13,45 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+
+    <div class="py-20 light:bg-gray-800 flex flex-col items-center">
+        <div class="text-center mb-12">
+            <h6 class="text-3xl font-bold mb-4">How do people feel about this topic?</h6>
+        </div>
+        <section class="grid gap-20 md:grid-cols-2 p-4 md:p-8 max-w-5xl mx-auto w-full">
+            <div class="p-6 bg-white shadow rounded-2xl light:bg-gray-900">
+                <dl class="space-y-2">
+                    <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Agree</dt>
+                    <dd class="text-5xl font-light md:text-6xl light:text-white">20</dd>
+                    <dd class="flex items-center space-x-1 text-sm font-medium text-green-500 dark:text-green-400">
+                        <span>Up vote</span>
+                        <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M17.25 15.25V6.75H8.75"></path>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M17 7L6.75 17.25"></path>
+                        </svg>
+                    </dd>
+                </dl>
+            </div>
+    
+            <div class="p-6 bg-white shadow rounded-2xl light:bg-gray-900">
+                <dl class="space-y-2">
+                    <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">Disagree</dt>
+                    <dd class="text-5xl font-light md:text-6xl light:text-white">5</dd>
+                    <dd class="flex items-center space-x-1 text-sm font-medium text-red-500 dark:text-red-400">
+                        <span>Down Vote</span>
+                        <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M17.25 8.75V17.25H8.75"></path>
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                d="M17 17L6.75 6.75"></path>
+                        </svg>
+                    </dd>
+                </dl>
+            </div>
+        </section>
     </div>
+    
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+                        CSE JnU - Voting Panel
                     </div>
                 </header>
             @endisset
